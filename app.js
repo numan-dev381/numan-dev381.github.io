ec6e77c05b60014a8559a5dccc0d0722e35b58e7
Numan Nasir <numannasir3815@gmail.com>
2026-07-23 23:33:54 +0500

    app.js

---
+++ app.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-app.js";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDcPnYtLxbvWEE4XaqCpgA95cl4yW-LXDg",
+  authDomain: "numan-portfolio-ccaf7.firebaseapp.com",
+  projectId: "numan-portfolio-ccaf7",
+  storageBucket: "numan-portfolio-ccaf7.firebasestorage.app",
+  messagingSenderId: "587725901836",
+  appId: "1:587725901836:web:0d04e5dc0ec147b6255e29"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+console.log("Firebase Connected Successfully!");
