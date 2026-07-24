bb5842d8317ea5428bbb0506e35c66d86fb8ab0c
Numan Nasir <numannasir3815@gmail.com>
2026-07-24 15:33:34 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,36 @@
 const addProjectBtn = document.getElementById("addProjectBtn");
+const projectModal = document.getElementById("projectModal");
+const closeModal = document.getElementById("closeModal");
 
+// Open popup
 if (addProjectBtn) {
 
     addProjectBtn.addEventListener("click", function () {
 
-        alert("Project form will be added in the next step.");
+        projectModal.style.display = "flex";
 
     });
 
 }
+
+// Close popup using ×
+if (closeModal) {
+
+    closeModal.addEventListener("click", function () {
+
+        projectModal.style.display = "none";
+
+    });
+
+}
+
+// Close popup when clicking outside
+window.addEventListener("click", function (event) {
+
+    if (event.target === projectModal) {
+
+        projectModal.style.display = "none";
+
+    }
+
+});
