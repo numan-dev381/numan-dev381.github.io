061b9154ab2cf69293e45258ccff0910eaae9300
Numan Nasir <numannasir3815@gmail.com>
2026-07-25 15:53:16 +0500

    Update script.js

--- script.js
+++ script.js
@@ -33,30 +33,3 @@ if (projectImage) {
     });
 
 }
-.remove-file{
-
-    float:right;
-
-    background:#ef4444;
-
-    color:white;
-
-    border:none;
-
-    padding:6px 14px;
-
-    border-radius:8px;
-
-    cursor:pointer;
-
-    font-size:14px;
-
-    transition:.3s;
-
-}
-
-.remove-file:hover{
-
-    background:#dc2626;
-
-}
