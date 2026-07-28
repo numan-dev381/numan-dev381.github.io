0d0bf60cb96817bfcdd866cc8e0045b59773037c
Numan Nasir <numannasir3815@gmail.com>
2026-07-28 10:21:44 +0500

    Create firebase.js

---
+++ js/firebase.js
@@ -0,0 +1,28 @@
+// =========================================
+// Firebase Configuration
+// =========================================
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
+
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
+
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCKc0GMk8w_xikGGJCgW9lwXLCtkNYqlTko",
+    authDomain: "numan-portfolio-9eea5.firebaseapp.com",
+    projectId: "numan-portfolio-9eea5",
+    storageBucket: "numan-portfolio-9eea5.firebasestorage.app",
+    messagingSenderId: "904457810296",
+    appId: "1:904457810296:web:f92a45a5b1f86fb2b8188d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Firebase Services
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+// Export Services
+export { auth, db };
