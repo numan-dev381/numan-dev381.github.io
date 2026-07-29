63206bf9145839cdb6b6342e5439a876c673ed9a
Numan Nasir <numannasir3815@gmail.com>
2026-07-29 10:47:18 +0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -80,3 +80,21 @@ loginForm.addEventListener("submit", async (e) => {
     await login(email, password);
 
 });
+const password = document.getElementById("password");
+const togglePassword = document.getElementById("togglePassword");
+
+togglePassword.addEventListener("click", () => {
+
+    if (password.type === "password") {
+
+        password.type = "text";
+        togglePassword.textContent = "Hide";
+
+    } else {
+
+        password.type = "password";
+        togglePassword.textContent = "Show";
+
+    }
+
+});
